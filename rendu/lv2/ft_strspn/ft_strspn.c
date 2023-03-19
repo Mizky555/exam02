a5c73a8280d6931b20ab16b69f1a7b41bd3db238
Thanaporn Sirirakwongsa <tsirirak@z3t3c2.42bangkok.com>
2023-03-19 17:57:39 +0700

    teenoi

---
+++ rendu/lv2/ft_strspn/ft_strspn.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return ((char *)s);
+		++s;
+	}
+	return (0);
+}
+
+size_t	ft_strspn(const char *s, const char *accept)
+{
+	size_t i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (ft_strchr(accept, s[i]) == 0)
+			break;
+		++i;
+	}
+	return (i);
+}
+
+int	main()
+{
+	printf("strspn = %lu\n",ft_strspn("Hello","Ho" ));
+}
+
+
+// int	main()
+// {
+
+// 	printf("sizte = %lu\n",strspn("hello","ello"));
+// }
