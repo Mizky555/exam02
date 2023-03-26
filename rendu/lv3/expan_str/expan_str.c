0ee6f305cd2bcff9444d2eb06ed5d3fa987f6ef5
Mizky555 <opaltales_@windowslive.com>
2023-03-26 23:54:42 +0700

    ha

---
+++ rendu/lv3/expan_str/expan_str.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int i = 0;
+
+    if (argc == 2)
+    {
+        while (argv[1][i] == ' ' || argv[1][i] == '\t')
+            i++;
+        while (argv[1][i])
+        {
+            if (argv[1][i] == ' ' || argv[1][i] == '\t')
+            {
+                while (argv[1][i] == ' ' || argv[1][i] == '\t')
+                    i++;
+                    write (1, "\t", 1);
+            }
+            write (1, &argv[1][i], 1);
+            i++;
+        }
+    }
+    write (1, "\n", 1);
+}
